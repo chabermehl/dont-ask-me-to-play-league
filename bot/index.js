432efd8eb2ecc7f1940dfbd9828411b4a83b54cf
chabermehl <clp.habermehl@gmail.com>
2021-04-23 18:39:19 -0600

    Restructure + cleanup commands

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const {prefix} = require('./config.json');
+const {prodPrefix, devPrefix} = require('./config.json');
+const prefix = process.env.BOT_ENV === 'production' ? prodPrefix : devPrefix;
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -20,7 +21,7 @@ client.on('message', (msg) => {
     msg.channel.send(
       `The nearest possible date at which I will consider playing League of Legends (LoL) at is: \n\n${addDays.toString()}`
     );
-  }else if(msg.content === `${prefix}coorsTime`){
+  } else if (msg.content === `${prefix}coorsTime`) {
     const initialTimeFrame = new Date(2021, 9, 20);
     daysAdded--;
     const rmvDays = new Date(
