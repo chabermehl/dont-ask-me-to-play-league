94d861866c98e76c6f5b612afb1c800e5542c3f2
chabermehl <clp.habermehl@gmail.com>
2021-05-01 15:00:33 -0600

    Add timeout to prevent spam

--- bot/index.js
+++ bot/index.js
@@ -8,6 +8,7 @@ const prefix = process.env.BOT_ENV === 'production' ? prodPrefix : devPrefix;
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
@@ -28,16 +29,44 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', async (message) => {
+client.on('message', (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift();
+  const commandString = args.shift();
 
-  if (!client.commands.has(command)) return;
+  if (!client.commands.has(commandString)) return;
+
+  const {commands, cooldowns} = client;
+
+  const command = commands.get(commandString);
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = command.cooldown * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(
+        `Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${
+          command.name
+        }\` command.`
+      );
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
   try {
-    client.commands.get(command).execute(message, args, dbClient, {prefix});
+    command.execute(message, args, dbClient, {prefix});
   } catch (error) {
     console.error(error);
     message.reply('There was an error executing the command.');
